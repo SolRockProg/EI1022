f1c4ab55f73fc1441fcaebf5cab3cd585a722853
jaume <al375837@uji.es>
2019-11-01 18:31:56 +0100

    oo

--- Problemas/Sesion3_voraces/Entregable2.py
+++ Problemas/Sesion3_voraces/Entregable2.py
@@ -1,10 +1,13 @@
 from algoritmia.datastructures.digraphs import UndirectedGraph
 from typing import *
 
+from algoritmia.utils import argmax
+
+
 def load_labyrinth(filename: str):
     def string_to_tuple(linea: str) -> Tuple:
-        linea=linea.split()
-        return (int(linea[0]), int(linea[1])),(int(linea[2]), int(linea[3]))
+        linea = linea.split()
+        return (int(linea[0]), int(linea[1])), (int(linea[2]), int(linea[3]))
 
     with open(filename, "r") as f:
         edges = []
@@ -17,6 +20,7 @@ def load_labyrinth(filename: str):
 def algoritmo1(g: UndirectedGraph) -> Tuple[int, Dict[Tuple[int, int], int]]:
     vertices = sorted(g.V, key=lambda x: (-len(g.succs(x)), -x[0], -x[1]))
     colors = []
+
     dic = {v: -1 for v in g.V}
     n_colores = 0
     for v in vertices:
@@ -33,15 +37,22 @@ def algoritmo1(g: UndirectedGraph) -> Tuple[int, Dict[Tuple[int, int], int]]:
             dic[v] = n_colores
             colors.append(n_colores)
             n_colores += 1
-    return (n_colores, dic)
+    return n_colores, dic
+
+
+def _vecinos_pintados(g: UndirectedGraph, v: Tuple[int, int], dic: dict):
+    return len([i for i in g.succs(v) if dic[i] != -1])
 
 
 def algoritmo2(g: UndirectedGraph) -> Tuple[int, Dict[Tuple[int, int], int]]:
-    vertices = set(g.V)
+    vertices=set()
+    for v in g.V:
+        vertices.add(v)
     colors = []
     dic = {v: -1 for v in g.V}
     n_colores = 0
-    for v in vertices:
+    while len(vertices) > 0:
+        v = argmax(vertices, fn=lambda x: (-_vecinos_pintados(g, x, dic), -len(g.succs(v)), -x[0], -x[1]))
         colores_vecinos = set()
         for vecino in g.succs(v):
             color = dic[vecino]
@@ -55,10 +66,17 @@ def algoritmo2(g: UndirectedGraph) -> Tuple[int, Dict[Tuple[int, int], int]]:
             dic[v] = n_colores
             colors.append(n_colores)
             n_colores += 1
-    return (n_colores, dic)
-if __name__=="__main__":
-    g=load_labyrinth(r"C:\Users\carlo\PycharmProjects\EI1022\Problemas\Sesion3_voraces\test-e2\graph-31-51.prob")
-    N,M=algoritmo1(g)
+        #print(v,_vecinos_pintados(g, v, dic))
+        vertices.remove(v)
+
+
+    return n_colores, dic
+
+
+if __name__ == "__main__":
+    g = load_labyrinth(
+        r"C:\Users\jaume\PycharmProjects\EI1022_boletines\Problemas\Sesion3_voraces\test-e2\graph-31-51.prob")
+    N, M = algoritmo2(g)
     print(N)
     for v in M.keys():
-        print(v,M[v])
+        print(v[0], v[1], M[v])
