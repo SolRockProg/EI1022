28eb73f92389beb4b601c91b827c6cbd097ffdb9
jaume <al375837@uji.es>
2019-11-05 19:31:06 +0100

    mejora en algoritmo 2

--- Problemas/Sesion3_voraces/Entregable2.py
+++ Problemas/Sesion3_voraces/Entregable2.py
@@ -40,23 +40,35 @@ def algoritmo1(g: UndirectedGraph) -> Tuple[int, Dict[Tuple[int, int], int]]:
     return n_colores, dic
 
 
-def _vecinos_pintados(g: UndirectedGraph, v: Tuple[int, int], dic: dict):
+def _vecinos_pintados(g: UndirectedGraph, vertices: set, dic: dict)->dict:
+    resultado = dict()
+    for v in vertices:
+        n = 0  # numero de vecinos pintados
+        colores = set()
+        for vecino in g.succs(v):
+            color = dic[vecino]
+            if color != -1:
+                colores.add(color)
+                n += 1
+        resultado[v] = n, colores
+
+    return resultado
+
+
+def _old_vecinos_pintados(g: UndirectedGraph, vertices: set, dic: dict):
     return len([i for i in g.succs(v) if dic[i] != -1])
 
 
 def algoritmo2(g: UndirectedGraph) -> Tuple[int, Dict[Tuple[int, int], int]]:
-    vertices=set()
+    vertices = set()
     for v in g.V:
         vertices.add(v)
     dic = {v: -1 for v in g.V}
     n_colores = 0
     while len(vertices) > 0:
-        v = argmax(vertices, fn=lambda x: (_vecinos_pintados(g, x, dic), len(g.succs(x)), x[0], x[1]))
-        colores_vecinos = set()
-        for vecino in g.succs(v):
-            color = dic[vecino]
-            if color != -1:
-                colores_vecinos.add(color)
+        vecinos=_vecinos_pintados(g, vertices, dic)
+        v = argmax(vertices, fn=lambda x: (vecinos[x][0], len(g.succs(x)), x[0], x[1]))
+        colores_vecinos = vecinos[v][1]
         for color in range(n_colores):
             if color not in colores_vecinos:
                 dic[v] = color
@@ -64,7 +76,6 @@ def algoritmo2(g: UndirectedGraph) -> Tuple[int, Dict[Tuple[int, int], int]]:
         else:
             dic[v] = n_colores
             n_colores += 1
-        #print(v,_vecinos_pintados(g, v, dic))
         vertices.remove(v)
     return n_colores, dic
 
@@ -77,13 +88,13 @@ if __name__ == "__main__":
         elif sys.argv[1] == '-2':
             N, M = algoritmo2(g)
         else:
-            print(sys.argv[1],"no es un argumento válido se debe poner -1 o -2")
+            print(sys.argv[1], "no es un argumento válido se debe poner -1 o -2")
             sys.exit(-1)
         print(N)
-        for v in sorted(M.keys(),key=lambda x:(x[0],x[1])):
+        for v in sorted(M.keys(), key=lambda x: (x[0], x[1])):
             print(v[0], v[1], M[v])
         if sys.argv[3] == "-g":
-            viewer = GraphColoring2DViewer(g, M, window_size=(1000, 400))
+            viewer = GraphColoring2DViewer(g, M, window_size=(1000, 600))
             viewer.run()
     else:
         print("Uso: Entregable.py <-1 o -2> <path>")
