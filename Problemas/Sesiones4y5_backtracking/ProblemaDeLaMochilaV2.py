23687e6bab29866bff8a50bc2f04959062b254b5
SolRockProg <carlosmora99@gmail.com>
2019-11-05 18:53:40 +0100

    Problema de la mochila y de la moneda //Backtracking

--- Problemas/Sesiones4y5_backtracking/ProblemaDeLaMochilaV2.py
+++ Problemas/Sesiones4y5_backtracking/ProblemaDeLaMochilaV2.py
@@ -1,14 +1,16 @@
 from Utils.bt_scheme import PartialSolutionWithOptimization, BacktrackingOptSolver, State, Solution
 from typing import *
 from random import random, seed
+from time import time
 
 
 def knapsack_solve(weights, values, capacity):
     class KnapsackPS(PartialSolutionWithOptimization):
-        def __init__(self, solution=(), suma_pesos=0):  # IMPLEMENTAR: Añade los parámetros que tú consideres
+        def __init__(self, solution=(), suma_pesos=0, suma_valores=0):  # IMPLEMENTAR: Añade los parámetros que tú consideres
             self.solution = solution
             self.n = len(solution)
             self.suma_pesos = suma_pesos
+            self.suma_valores=suma_valores
 
         def is_solution(self) -> bool:  # IMPLEMENTAR
             return self.n == len(values) and self.suma_pesos <= capacity
@@ -19,15 +21,15 @@ def knapsack_solve(weights, values, capacity):
         def successors(self) -> Iterable["KnapsackPS"]:  # IMPLEMENTAR
             if self.n >= len(values):
                 return []
-            yield KnapsackPS(self.solution + (0,), self.suma_pesos)
+            yield KnapsackPS(self.solution + (0,), self.suma_pesos, self.suma_valores)
             if self.suma_pesos + weights[self.n] <= capacity:
-                yield KnapsackPS(self.solution + (1,), self.suma_pesos + weights[self.n])
+                yield KnapsackPS(self.solution + (1,), self.suma_pesos + weights[self.n], self.suma_valores+values[self.n])
 
         def state(self) -> State:  # IMPLEMENTAR
             return self.n, self.suma_pesos
 
         def f(self) -> Union[int, float]:  # IMPLEMENTAR
-            return -sum(v*values[i] for i,v in enumerate(self.solution))
+            return -self.suma_valores
 
     initialPS = KnapsackPS()  # IMPLEMENTAR: Añade los parámetros que tú consideres
     return BacktrackingOptSolver.solve(initialPS)
@@ -43,9 +45,12 @@ def create_knapsack_problem(num_objects: int) -> Tuple[Tuple[int, ...], Tuple[in
 
 # Programa principal ------------------------------------------
 if __name__ == "__main__":
-    #W, V, C = [1, 4, 2, 3], [2, 3, 4, 2], 7  # SOLUCIÓN: Weight=7,    Value=9
-    W, V, C = create_knapsack_problem(30)     # SOLUCIÓN: Weight=6313, Value=11824
+    # W, V, C = [1, 4, 2, 3], [2, 3, 4, 2], 7  # SOLUCIÓN: Weight=7,    Value=9
+    W, V, C = create_knapsack_problem(30)  # SOLUCIÓN: Weight=6313, Value=11824
+    start_time = time()
     for sol in knapsack_solve(W, V, C):
-        print("Weight: "+str(sum(v*W[i] for i,v in enumerate(sol)))+ " Value: "+str(sum(v*V[i] for i,v in enumerate(sol))))
-      # print(sol)
-    print("\n<TERMINADO>")
+        print("Weight: " + str(sum(v * W[i] for i, v in enumerate(sol))) + " Value: " + str(
+            sum(v * V[i] for i, v in enumerate(sol))))
+    # print(sol)
+    finish_time = time()
+    print("\n<TERMINADO> Tiempo de ejecucion: ", finish_time - start_time)
