8b9dae16df7bcf5a14a3e54c9a3113b93eda34cc
SolRockProg <carlosmora99@gmail.com>
2019-11-12 00:27:59 +0100

    Entregables 3

--- Problemas/Sesion3_voraces/PruebaDiccionarioPrioridad.py
+++ Problemas/Sesion3_voraces/PruebaDiccionarioPrioridad.py
@@ -1,8 +1,6 @@
+import math
+
 from algoritmia.datastructures.prioritymaps import HeapMap
 
 if __name__ == "__main__":
-    list=[1, 2, 3, 4]
-    k = set(list)
-    print(k)
-    var = k - {3}
-    print(k)
+    print(math.floor(9/10))
--- Problemas/Sesiones4y5_backtracking/Entregable3.py
+++ Problemas/Sesiones4y5_backtracking/Entregable3.py
@@ -1,3 +1,4 @@
+import math
 from typing import *
 from copy import deepcopy
 
@@ -22,27 +23,37 @@ def crypto_solver(data: Tuple[str, ...]) -> Iterable:
         def successors(self) -> Iterable["PartialSolution"]:
             if self.n < len(letters):
                 for digit in self.digitos_disponibles:
-                    if self._factible():
+                    if self._factible(digit):
                         new_solution = deepcopy(self.solution)
                         new_solution[letters[self.n]] = digit
-                        yield CryptoAPS(new_solution, self.digitos_disponibles-{digit})
+                        yield CryptoAPS(new_solution, self.digitos_disponibles - {digit})
 
-        def _factible(self) -> bool:
+        def _factible(self, digit) -> bool:
+            nonlocal matrix
+            acarreo = 0
+            for col in range(len(matrix[0])):
+                if any(matrix[fila][col] not in self.solution for fila in range(len(matrix))):
+                    return True
+                suma = sum(self.solution[matrix[fil][col]] for fil in range(len(matrix) - 1)) + acarreo
+                acarreo = math.floor(suma/10)
+                if suma != self.solution[matrix[-1][col]]:
+                    return False
             return True
 
-
-    letters = letters_on_strings(data)
+    matrix, letters = letters_on_strings(data)
     initial_ps = CryptoAPS({}, set(i for i in range(1, 10)))
     return BacktrackingSolver.solve(initial_ps)
 
 
 def letters_on_strings(data):
     letters = []
-    for letter in range(-1, -len(argmax(data, lambda x: len(x))), -1):
+    matrix = [[None] * len(data[-1]) for i in range(len(data))]
+    for letter in range(-1, -len(data[-1]), -1):
         for word in range(len(data)):
+            matrix[word][letter] = data[word][letter]
             if data[word][letter] not in letters:
                 letters.append(data[word][letter])
-    return tuple(letters)
+    return matrix, tuple(letters)
 
 
 if __name__ == "__main__":
