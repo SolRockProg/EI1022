ff4277719d625cbabcda75fdedcc7ce03413a289
SolRockProg <carlosmora99@gmail.com>
2019-11-20 22:40:52 +0100

    SumaMaxima version simplificada

--- Problemas/Sesion7_DYV/SumaMaxima.py
+++ Problemas/Sesion7_DYV/SumaMaxima.py
@@ -1,28 +1,24 @@
 from typing import *
 
+def acumulator_calc(inicio, fin , vector,  paso=1):
+    acumulator = 0
+    max_acum = vector[inicio]
+    indice = inicio
+    for i in range(inicio, fin, paso):
+        acumulator += vector[i]
+        if acumulator > max_acum:
+            max_acum = acumulator
+            indice = i+1
+    return indice, max_acum
+
 def smax(vector: List[int], b: int, e: int) -> (int, int, int):
     if e - b == 1:
         return b, e, vector[b]
     half = (b + e) // 2
     b_izq, e_izq, acumulation_izq = smax(vector, b, half)
     b_der, e_der, acumulation_der = smax(vector, half, e)
-    acumulator = 0
-    max_acum_izq = vector[half-1]
-    indice_izq = half-1
-    for i in range(half-1, b-1, -1):
-        acumulator += vector[i]
-        if acumulator > max_acum_izq:
-            max_acum_izq = acumulator
-            indice_izq = i
-    indice_der = half
-    max_acum_der = vector[half]
-    acumulator=0
-    for i in range(half, e):
-        acumulator += vector[i]
-        if acumulator > max_acum_der:
-            max_acum_der = acumulator
-            indice_der = i+1
-
+    indice_izq, max_acum_izq = acumulator_calc(half-1, b-1, vector, paso=-1)
+    indice_der, max_acum_der=acumulator_calc(half, e, vector)
     best = max(acumulation_der, acumulation_izq, max_acum_izq + max_acum_der)
     if best == acumulation_izq:
         return b_izq, e_izq, acumulation_izq
