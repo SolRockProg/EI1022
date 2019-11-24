aafff6ac8c89f77e856469dafe9bf2de3c5a2194
SolRockProg <carlosmora99@gmail.com>
2019-11-24 17:21:23 +0100

    Punto Fijo arreglado +  version recursiva

--- Problemas/Sesion7_DYV/PuntoFijo.py
+++ Problemas/Sesion7_DYV/PuntoFijo.py
@@ -1,9 +1,23 @@
 from typing import *
 
-# Hacer la version recursiva
+
+# Version recursiva
+def punto_fijo_rec(vector: List[int], b: int, e: int) -> Optional:
+    if e - b == 1:
+        return b if vector[b] == b else None
+    if vector[b] > b:
+        return None
+    half = (b + e) // 2
+    if vector[half] > half:
+        return punto_fijo_rec(vector, b, half)
+    elif vector[half] < half:
+        return punto_fijo_rec(vector, half, e)
+    else:
+        return half
+
 
 # Version iterativa
-def punto_fijo(vector) -> Optional:
+def punto_fijo(vector: List[int]) -> Optional:
     indice1 = 0
     indice2 = len(vector)
     while not indice2 - indice1 == 1:
@@ -11,14 +25,14 @@ def punto_fijo(vector) -> Optional:
             return None
         half = (indice1 + indice2) // 2
         if vector[half] > half:
-            indice2 = half + 1
+            indice2 = half
         elif vector[half] < half:
             indice1 = half
         else:
             return half
-    return None
+    return indice1 if vector[indice1] == indice1 else None
 
 
 if __name__ == "__main__":
-    vector = [-3, -2, 2, 4, 6, 10]
+    vector = [0, 2]
     print(punto_fijo(vector))
