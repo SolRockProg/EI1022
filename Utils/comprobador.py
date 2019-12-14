20d1e0d856bd5a6aeda041ffb50a8a4ecfac3e4d
SolRockProg <carlosmora99@gmail.com>
2019-12-14 23:44:21 +0100

    Punto Fijo arreglado +  version recursiva

--- Utils/comprobador.py
+++ Utils/comprobador.py
@@ -23,10 +23,10 @@ def comparar_ficheros(file1: str, file2: str):
         print(file1.split("\\")[-1] + ": " + str(len(vector_lineas)) + " lineas")
         print(file2.split("\\")[-1] + ": " + str(len(vector_lineas2)) + " lineas")
     else:
-        esta_bien=True
+        esta_bien = True
         for i, line in enumerate(vector_lineas):
             if line != vector_lineas2[i]:
-                esta_bien=False
+                esta_bien = False
                 print("Linea " + str(i) + ":\n\t+ " + str(line) + "\n\t- " + str(vector_lineas2[i]))
         if esta_bien:
             print("TODO CORRECTO")
@@ -35,10 +35,11 @@ def comparar_ficheros(file1: str, file2: str):
 def muchos_ficheros(f, path):
     os.chdir(path)
     for input_file in glob.glob("*.i"):
-        os.system(f+" "+input_file+ "> solucion.txt")
+        os.system(f + " " + input_file + "> solucion.txt")
         output_file = input_file[:-1] + "o"
         comparar_ficheros("solucion.txt", output_file)
 
 
 if __name__ == "__main__":
-    muchos_ficheros("Entregable5.py", r"C:\Users\carlo\PycharmProjects\EI1022git\Problemas\Sesion8y9_PDinamica\Entregable")
+    muchos_ficheros("Entregable5.py",
+                    r"C:\Users\carlo\PycharmProjects\EI1022git\Problemas\Sesion8y9_PDinamica\Entregable")
